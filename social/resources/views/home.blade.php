131426575cf0e1749b05d92887449e9e8f2e539a
Caio César <br.caiorecife@cesarleitao.com.br>
2020-02-06 18:23:58 -0300

    add routes

--- social/resources/views/home.blade.php
+++ social/resources/views/home.blade.php
@@ -3,12 +3,15 @@
 @section('content')
 <div class="container">
     <div class="row">
+        <!--
         <div class="col-3 p-5">
             <img src="https://instagram.frec3-1.fna.fbcdn.net/v/t51.2885-19/s150x150/53577799_442141352992181_6086663970687549440_n.jpg?_nc_ht=instagram.frec3-1.fna.fbcdn.net&_nc_ohc=ZE5hAxvmvKAAX-_dfvf&oh=d2673190d370b6688f50003a916ac246&oe=5ED91FA6" style='border-radius: 50%;'/>
         </div>
-        <a class="col-9 p-5">
-            <div><h1>caio4code</h1></div>
+        -->
+        <a class="p-5">
+            <div><h1>{{ $user->name }}</h1></div>
 
+            <!--
             <div class="d-flex">
             <div class="pr-5"><strong>283</strong> posts</div>
             <div class="pr-5"><strong>976K</strong> followers</div>
@@ -18,34 +21,16 @@
 
             </div>
 
-            <div class="pt-4"><strong>Caio César</strong></div>
-            <div>I want to change the world with my projects. High School Student</div>
+            <div class="pt-4"><strong>High School Student</strong></div>
+            <div>I want to change the world with my projects.</div>
 
         </div>
-
+-->
     <div class="row">
         <div class="col-4 p-4">
-            <img src="https://instagram.frec3-2.fna.fbcdn.net/v/t51.2885-15/e35/82393505_797386860764378_8012807300068191857_n.jpg?_nc_ht=instagram.frec3-2.fna.fbcdn.net&_nc_cat=102&_nc_ohc=Ylm7PDjkpWEAX95zECA&oh=26bc025db9e8cd5b2adae3fd9499ac2b&oe=5EBE784E" style="border-radius: 5px" class="w-100"/>
+            <img src="" style="border-radius: 5px" class="w-100"/>
 
         </div>
-        <div class="col-4 p-4">
-            <img src="https://instagram.frec3-2.fna.fbcdn.net/v/t51.2885-15/e35/82393505_797386860764378_8012807300068191857_n.jpg?_nc_ht=instagram.frec3-2.fna.fbcdn.net&_nc_cat=102&_nc_ohc=Ylm7PDjkpWEAX95zECA&oh=26bc025db9e8cd5b2adae3fd9499ac2b&oe=5EBE784E" style="border-radius: 5px" class="w-100"/>
-
-        </div>
-        <div class="col-4 p-4">
-            <img src="https://instagram.frec3-2.fna.fbcdn.net/v/t51.2885-15/e35/82393505_797386860764378_8012807300068191857_n.jpg?_nc_ht=instagram.frec3-2.fna.fbcdn.net&_nc_cat=102&_nc_ohc=Ylm7PDjkpWEAX95zECA&oh=26bc025db9e8cd5b2adae3fd9499ac2b&oe=5EBE784E" style="border-radius: 5px" class="w-100"/>
-
-        </div>
-        <div class="col-4 p-4">
-            <img src="https://instagram.frec3-2.fna.fbcdn.net/v/t51.2885-15/e35/82393505_797386860764378_8012807300068191857_n.jpg?_nc_ht=instagram.frec3-2.fna.fbcdn.net&_nc_cat=102&_nc_ohc=Ylm7PDjkpWEAX95zECA&oh=26bc025db9e8cd5b2adae3fd9499ac2b&oe=5EBE784E" style="border-radius: 5px" class="w-100"/>
-
-        </div>
-        <div class="col-4 p-4">
-            <img src="https://instagram.frec3-2.fna.fbcdn.net/v/t51.2885-15/e35/82393505_797386860764378_8012807300068191857_n.jpg?_nc_ht=instagram.frec3-2.fna.fbcdn.net&_nc_cat=102&_nc_ohc=Ylm7PDjkpWEAX95zECA&oh=26bc025db9e8cd5b2adae3fd9499ac2b&oe=5EBE784E" style="border-radius: 5px" class="w-100"/>
-
-        </div>
-
-    </div>
 
 
     </div>
